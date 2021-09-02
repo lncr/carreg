e8a0cfab06ee47909c70f7efe1c384dd99796a9b
Bektur <b.soltobaev@disoft.dev>
2021-09-02 19:52:08 +0600

    Add directory app

---
+++ directory/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Mark(models.Model):
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='marks')
+    name = models.CharField(max_length=128)
