48f2611702df20b978f41ea394f801330ad69665
Bektur <b.soltobaev@disoft.dev>
2021-09-07 18:58:59 +0600

    Resolve conflicts

--- cars/views.py
+++ cars/views.py
@@ -13,10 +13,7 @@ from accounts.tasks import send_mail_to_worker
 class CarViewSet(ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
-<<<<<<< HEAD
     permission_classes = [IsAuthenticated & DiversePermission, ]
-=======
-    permission_classes = [IsAuthenticated, DiversePermission, ]
 
     @action(methods=['get', ], detail=True)
     def email_reminder(self, request, *args, **kwargs):
@@ -25,4 +22,3 @@ class CarViewSet(ModelViewSet):
             send_mail_to_worker.delay()
             return Response('You have sent email to remind about this car')
         return Response('This car does not have worker associated with it', status=status.HTTP_404_NOT_FOUND)
->>>>>>> 6b34ce5bc9396cee3e5b86727c582d4afabf2e50
