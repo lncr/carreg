f1c159c465e62126539e1dc18227518da9a88c72
Bektur <b.soltobaev@disoft.dev>
2021-09-10 19:40:30 +0600

    Fix docker-compose

--- carreg/settings.py
+++ carreg/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'NAME': env('POSTGRES_DATABASE'),
         'USER': env('POSTGRES_USER'),
         'PASSWORD': env('POSTGRES_PASSWORD'),
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': 5432,
     }
 }
